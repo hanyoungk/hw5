7556d163938278bdd20a7ef6e11a7f8930499da4
Hanyoung Kim <hanykim029@gmail.com>
2023-04-08 02:15:12 -0700

    finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,10 +20,34 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int> workerShifts,
+    int row, int col);
 
 
 // Add your implementation of schedule() and other helper functions here
 
+
+/**
+ * @brief Produces a work schedule given worker availability,
+ *        the number of needed workers per day, and the maximum 
+ *        shifts any single worker is allowed. Returns true
+ *        and the valid schedule if a solution exists, and false
+ *        otherwise. 
+ * 
+ * @param [in]  avail n x k vector of vectors (i.e. matrix) of the availability
+ *                    of the k workers for each of the n days
+ * @param [in]  dailyNeed Number of workers needed per day (aka d)
+ * @param [in]  maxShifts Maximum shifts any worker is allowed over 
+ *                        the n day period (aka m)
+ * @param [out] sched n x d vector of vectors indicating the d workers
+ *                    who are scheduled to work on each of the n days
+ * @return true if a solution exists; sched contains the solution
+ * @return false if no solution exists; sched is undefined (can be anything)
+ */
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -37,8 +61,72 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    //keep track of row and col of avail matrix (vector of vector)
+    int row = 0; //day #
+    int col = 0; //worker ID
 
+    //create a data structure to keep track of # shifts each worker has worked
+    // worker IDs:         0  1  2  3  4
+    // # of shifts worked [0][0][0][0][0]
+    vector<int> workerShifts;
+    for(size_t i = 0; i < avail[0].size(); i++) {
+        workerShifts.push_back(0);
+    }
+
+    //set of sched for # of days (resizing)
+    sched.resize(avail.size());
+
+
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, workerShifts, row, col);
+}
+
+bool scheduleHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int> workerShifts,
+    int row, int col
+)
+{
+
+    int currentWorker = 0;
+
+    //base case: reached the end of matrix
+    if(row == avail.size()) {
+        return true;
+    }
+
+    //if in the last column or 
+    if(col > avail[0].size() - 1 || sched[row].size() == dailyNeed) {
+        //go to the next row & reset the column
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, workerShifts, row+1, 0);
+    }
+
+
+    for(size_t id = 0; id < avail[0].size(); id++) { //for the entire row
+        //if worker ID(col) has LESS than m && # worker for day(row) has LESS than dailyNeed
+
+        // if worker is available & worker's shift is < m & # workers assigned for day has LESS than dailyNeed
+        if(avail[row][id] && workerShifts[id] < maxShifts && sched[row].size() < dailyNeed) {
+
+            // check if the worker is alr in the scheudle
+            if(find(sched[row].begin(), sched[row].end(), id)  == sched[row].end())
+            {
+                //add the worker to the shift
+                sched[row].push_back(id);
+                //update the worker's shift amount
+                workerShifts[id]++;
+                currentWorker = id;
+                if(scheduleHelper(avail, dailyNeed, maxShifts, sched, workerShifts, row, col+1))
+                    return true;
+                sched[row].pop_back();
+                workerShifts[currentWorker]--;
+            }
+        }
+    }
+    return false;
+
+    
 
 
 }
-
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(const string& in, const string& floating, const set<string>& dict, set<string>& answers, int idx);
 
 
 // Definition of primary wordle function
@@ -21,8 +22,108 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+
+    //create a set of string for all the possible answers
+    set<string> answers;
+
+    //keep track of current spot
+    int idx = 0;
+
+    //using "in" put in "floating" in the blanks first before the alphabet
+
+    //first call to recursive function
+    wordleHelper(in, floating, dict, answers, idx);
+
+    return answers;
+
 
 }
 
 // Define any helper functions here
+void wordleHelper(const string& in, const string& floating, const set<string>& dict, set<string>& answers, int idx) 
+{
+    // cout << "yellow when recursed: " << floating << endl;
+
+    //base case: used all of the floating letter AND at the last letter
+    if(floating.size() == 0 && idx == in.size()) {
+        
+        set<string>::iterator it = dict.find(in);
+        //check if the completed word is in the dictionary
+        if(it != dict.end()) {
+            //if it is, add the word to the set of answers
+            answers.insert(in);
+        }
+        return; //if word has no more blanks, return (if found)/undo (if not found)
+    }
+
+
+    //used as a placeholder
+    string yellow = floating;
+    string green = in;
+
+    //count how many letters are blank
+    size_t blank = 0;
+    for(size_t i = 0; i < green.size(); i++) {
+        if(green[i] == '-') 
+            blank++;
+    }
+
+    //case 1: if the current index is blank (no letter)
+    if(green[idx] == '-') { 
+        
+        //case 1a: if the number of blank spaces == yellow letters
+        if(blank == yellow.size()) {
+            //then it should have those yellow letters 
+            for(size_t i = 0; i < yellow.size(); i++) {
+                green[idx] = yellow[i]; //insert the letter in the blank space
+                
+                int indexPos = i;
+                yellow.erase(indexPos, 1); //delete the letter from yellow bank after it was assigned
+
+                //recurse to the next index
+                wordleHelper(green, yellow, dict, answers, idx+1);
+
+                //reset the yellow letters for other blanks
+                yellow = floating;
+
+            }
+        }
+        
+        //case 1b: if there are more blanks than yellow letters
+        else if(blank > yellow.size()) { //if there are more blanks than yellow letters
+
+            //loop over the entire alphabet
+            for(size_t i = 'a'; i <= 'z'; i++) {
+                green[idx] = i;
+
+                if(yellow.find(i) != string::npos) { //if alphabet is a yellow letter
+                    // cout << "inside if yellow.find(i)" << endl;
+                    int indexPos = yellow.find(i);
+                    yellow.erase(indexPos,1); //delete the letter from yellow bank after it was assigned (green[idx] = i;)
+                    
+                    //recurse
+                    wordleHelper(green, yellow, dict, answers, idx+1);
+                    
+                    //reset the yellow letters for other blanks
+                    yellow = floating;
+                }
+                else { //only recurse, no yellow letter to delete
+                    wordleHelper(green, yellow, dict, answers, idx+1); 
+                }
+            }
+
+        }
+        else { //more floating than blanks
+            return;
+        }
+
+
+    }
+    //case 2: if the current index is filled
+    else { //already has a letter, so go onto the next index
+        wordleHelper(green, yellow, dict, answers, idx+1);
+    }
+
+    return;
+
+}
